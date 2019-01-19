cef9f5934a0afa0a4c0c74b3f1d91b60d2245ecd
LemuelKL <lemuellee.kl@gmail.com>
2019-01-19 15:16:53 +0800

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -1,11 +1,32 @@
 #ifndef BUBBLE_H
 #define BUBBLE_H
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 class bubble
 {
 public:
-    bubble();
+    bubble(std::vector<std::vector<cv::Point>>);
+
+    bool isBlackened();
+    void setBlackened(bool);
+
+    int cx();
+    int cy();
+    int r();
+    int optionNum();
+    void setOptionNum(int);
+
+private:
+    int _cx;
+    int _cy;
+    int _r;
+    int _optionNum;
+    bool _isBlackened;
+
 };
 
 #endif // BUBBLE_H
